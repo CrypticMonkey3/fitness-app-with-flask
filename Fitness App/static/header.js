d33bacb5890c03a4272651ec330c229cf7621279
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-04-18 00:22:03 +0100

    Update for header transition effect. Need to resolve why script gets loaded with MIME type ("text/plain") rather than ("text/javascript")

---
+++ Fitness App/static/header.js
@@ -0,0 +1,22 @@
+function stick_header() {
+    /*
+    Changes the css of elements identified as "sticky header".
+    :return: None
+    */
+    var header = document.getElementById("sticky_header");
+    var logo = document.getElementById("sub-menu icon");
+
+    if (window.pageYOffset > 0) {  // if the position of the page is not at the top
+        header.classList.add("sticky");  // appends the sticky class onto the sticky_header
+        logo.classList.remove("enlarge_logo");
+        logo.classList.add("shrink_logo");
+        console.log("stuck");
+    }
+    else {
+        header.classList.remove("sticky");  // removes the sticky class from the sticky_header
+        logo.classList.remove("shrink_logo");
+        logo.classList.add("enlarge_logo");
+        console.log("unstuck");
+    }
+}
+
