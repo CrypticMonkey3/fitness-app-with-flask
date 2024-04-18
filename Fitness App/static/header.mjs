65c925ab626a92a02b4e9a9897fd89c30315e8b5
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-04-19 00:53:55 +0100

    Got some background things going on, will change the background pattern to something more noticeable.

--- Fitness App/static/header.mjs
+++ Fitness App/static/header.mjs
@@ -4,17 +4,17 @@ function stick_header() {
     :return: None
     */
     var header = document.getElementById("sticky_header");
-    var logo = document.getElementById("sub-menu icon");
+    var header_contents = document.getElementById("header_contents");
 
     if (window.pageYOffset > 0) {  // if the position of the page is not at the top
         header.classList.add("sticky");  // appends the sticky class onto the sticky_header
-        logo.classList.remove("enlarge_logo");
-        logo.classList.add("shrink_logo");
+        header_contents.classList.remove("enlarge_header");
+        header_contents.classList.add("shrink_header");
     }
     else {
         header.classList.remove("sticky");  // removes the sticky class from the sticky_header
-        logo.classList.remove("shrink_logo");
-        logo.classList.add("enlarge_logo");
+        header_contents.classList.remove("shrink_header");
+        header_contents.classList.add("enlarge_header");
     }
 }
 
