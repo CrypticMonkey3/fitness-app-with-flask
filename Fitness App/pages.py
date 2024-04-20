ebd7da0fe3b736980332d456046da4255a427094
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-04-20 22:36:43 +0100

    Header sorted out

--- Fitness App/pages.py
+++ Fitness App/pages.py
@@ -12,3 +12,19 @@ def home():
 @bp.route("/about")
 def about():
     return render_template("pages/about.html")
+
+
+@bp.route("/friends")
+def friends():
+    return render_template("pages/friends.html")
+
+
+@bp.route("/online")
+def online():
+    return render_template("pages/online.html")
+
+
+@bp.route("/profile")
+def profile():
+    return render_template("pages/profile.html")
+
