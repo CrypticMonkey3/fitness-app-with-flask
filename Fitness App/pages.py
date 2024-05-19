44d205bfff11718d346050cb9b3cef42476956d1
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-05-19 18:44:01 +0100

    Started working on Register and Login window, next is the Flask input validation stage.

--- Fitness App/pages.py
+++ Fitness App/pages.py
@@ -9,6 +9,28 @@ def home():
     return render_template("pages/home.html")  # render_template expects parameter to be in a templates/ folder already.
 
 
-@bp.route("/login", methods=["GET", "POST"])
+@bp.route("/login")
 def login():
     return render_template("pages/login.html")
+
+
+@bp.route("/dob", methods=["GET", "POST"])
+def dob():
+    if request.method == "POST":
+        email = request.form.get("email")
+
+        print(email)
+        print(request.form)
+        print(list(request.form))
+        print(request.form["email"])
+        return render_template("pages/home.html")
+    return render_template("pages/dob.html")
+
+
+@bp.route("/menu", methods=["GET", "POST"])
+def menu():
+    return render_template("pages/menu.html")
+
+
+# NOTE FOR FUTURE: MAKE LOGIN AND REGISTRATION ON THE SAME FORM
+
