10437720f4383dfceeb5fa443acec04c2fa37c34
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-05-20 16:41:04 +0100

    Began working on some JS. Doesn't work at the moment, however, will be fixed in the future. Afterwards, need to establish an AJAX connection between Flask and the JS script.

--- Fitness App/pages.py
+++ Fitness App/pages.py
@@ -1,36 +1,48 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for, Response
+from typing import Union
 
 # blueprints are modules that help organise the structure of applications into subdirectories.
 bp = Blueprint("pages", __name__)
 
 
 @bp.route("/")
-def home():
+def home() -> str:
+    """
+    Landing page route
+    :return: str
+    """
     return render_template("pages/home.html")  # render_template expects parameter to be in a templates/ folder already.
 
 
 @bp.route("/login")
-def login():
+def login() -> str:
+    """
+    Login route
+    :return: str
+    """
     return render_template("pages/login.html")
 
 
 @bp.route("/dob", methods=["GET", "POST"])
-def dob():
+def dob() -> Union[str, Response]:
+    """
+    Date of Birth route
+    :return: Union[str, Response]
+    """
     if request.method == "POST":
-        email = request.form.get("email")
 
-        print(email)
         print(request.form)
         print(list(request.form))
-        print(request.form["email"])
-        return render_template("pages/home.html")
+        print(request.values)
+        return redirect(url_for("pages.home"))
+
     return render_template("pages/dob.html")
 
 
 @bp.route("/menu", methods=["GET", "POST"])
-def menu():
+def menu() -> str:
+    """
+    Menu route for user once logged in or registered.
+    :return: str
+    """
     return render_template("pages/menu.html")
-
-
-# NOTE FOR FUTURE: MAKE LOGIN AND REGISTRATION ON THE SAME FORM
-
